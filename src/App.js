88f71bbf5d986936c59037943964b2d43c3c7a73
X3n0nm4st3r0403 <erickj.cruzs@gmail.com>
2022-01-25 23:19:12 -0400

    [CREATE] App Search Image and Connect with the API Unsplash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Field, Form} from 'formik'
+import './header.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  console.log(photos);
+  return(
+    <header>
+      <Formik
+        initialValues={{ search: ''}}
+        onSubmit={async values => {
+          const response = await fetch(`https://api.unsplash.com/search/photos?per_pages=20&query=${values.search}`, { headers: {'Authorization': 'Client-ID -f9so7Frdcwp0GE_vVbjc7j2i2XcXkKM_sef_1M51Qg'} } )
+          const data = await response.json()
+          setPhotos(data.results)
+        }}
+      >
+
+        <Form>
+          <Field name='search' />
+        </Form>
+
+      </Formik>
+    </header>
+  )
 }
 
-export default App;
+export default App
+
+// Hasta ahora lo que se ha hecho es importar desde formik, los componentes Formik, Field and Form, con los cuales creamos el input de nuestra App.
+// A nuestros componente de Formik, le colocamos sus propiedades, las cuales son "initialValues (que son los valores iniciales de nuestros campo)", y "onSubmit (que es cuando le damos click al botón tipo submit o a la tecla de enter, que nos entrega los valores registrados en el campo de input)."
+// Luego solamente le dimos estilos a nuestro header para que tenga más presentación.
